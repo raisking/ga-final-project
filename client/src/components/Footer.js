a93581ba4f6c2a296404887a994cb17578a749e3
Prakash Rai <raiprakas007@gmail.com>
2017-11-20 17:42:36 -0500

    changed items container size

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,19 +8,6 @@ class Footer extends Component {
         return (
             <div className="footerCopyRight">
                 <div className="footerDivClass">
-                    <div className="footerInside">
-                        <h3>Social Media</h3>
-                        <span> <h4>Facebook</h4></span>
-                        <span> <h4>Instagram</h4></span>
-                        <span> <h4>Twitter</h4></span>
-                    </div>
-                    <div className="footerInside">
-                        <h3>Contact Us</h3>
-                        <h4>1-800-222-4444</h4>
-                        <h4>Email: buyandsellyourcar@gmail.com</h4>
-                        <h4>1215 Ernest W Barrett Pkwy NW, Kennesaw, GA 30144</h4>
-                    </div>
-                    <div></div>
                 </div>
                 <div className="developer">
                     <h4>Copyright@2017</h4>
